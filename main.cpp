7d09236da59039078b5d31c2e8794dbdd47dd360
chipaton <justinvineyard99@gmail.com>
2018-07-10 16:04:45 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "GPIO.h"
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    GPIO pinIn = new GPIO();
+    //put code that does things here
+}
